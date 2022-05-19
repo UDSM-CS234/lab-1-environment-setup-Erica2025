230662bf05c7d962907a89da148aac7da1539663
erica2025 <ericaairoking@gmail.com>
2022-05-19 18:53:34 +0200

    assignment1.java

---
+++ assignment1.java
@@ -0,0 +1,13 @@
+public class assignment1 {
+    public static void main(String[] args) {
+        double a=-9.81;
+        double vi=0;
+        double xi=0;
+        double xt;
+        int t=10;
+        xt=0.5*a*t*t+ vi*t+xi;
+        System.out.println("The value of xt is: " + xt);
+
+
+    }
+}
